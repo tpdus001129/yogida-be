a3b9d55183bd07e2a661a6de319866f722067199
kojeounhwa <rhwidghk125@naver.com>
2023-12-14 17:03:51 +0900

    Feat: 여행 세부 장소 스키마 작성

---
+++ src/models/schemas/travelPlace.js
@@ -0,0 +1,18 @@
+import { Schema } from 'mongoose';
+
+const travelPlaceSchema = new Schema({
+  // 여행 데이 (첫째날, 둘째날...)
+  travelDay: { type: Number, require: true },
+  // 장소
+  placeName: { type: String, require: true },
+  // 이미지
+  placeImageSrc: { type: String, require: true },
+  // 별점
+  starScore: { type: Number, require: true },
+  // 카테고리
+  category: { type: String, require: true },
+  // 거리 ( 맞는지 모름 수정 필요함)
+  distance: { type: Number, require: true },
+});
+
+export default travelPlaceSchema;
