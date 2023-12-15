685e4577b743e2eff9091adda197c00cca2c5569
kojeounhwa <rhwidghk125@naver.com>
2023-12-16 04:32:45 +0900

    Feat: 게시글 생성 시 조건 작성

--- src/services/postService.js
+++ src/services/postService.js
@@ -5,8 +5,8 @@ export async function getAllPosts() {
     const posts = await Post.find({}).exec();
 
     return posts;
-  } catch (error) {
-    throw new Error(error);
+  } catch (err) {
+    throw new Error(err);
   }
 }
 
@@ -24,6 +24,24 @@ export async function createPost(postProps) {
     return { status: 400, message: '게시글 내용이 없습니다.' };
   }
 
+  // 여행일정과 관련된 변수 설정
+  const singleScheduleLength = postProps.schedules.length;
+  const stDate = postProps.startDate;
+  const endDate = postProps.endDate;
+
+  // 세부 장소와 거리에 관련된 변수 설정
+  const singleSchedulePlaceCounts = postProps.schedules.map((location) => location.length - 1);
+  const distancesCounts = postProps.distances.map((distance) => distance.length);
+
+  // 여행일정 수 계산 (5월 30일 ~ 6월 2일이여도 계산 가능)
+  const dtMs = endDate.getTime() - stDate.getTime();
+  const travelDays = dtMs / (1000 * 60 * 60 * 24);
+
+  // 등록된 여행 날짜와 디데이 불일치 또는 세부 장소와 거리 갯수 불일치 시 오류 반환
+  if (singleScheduleLength !== travelDays || singleSchedulePlaceCounts.length !== distancesCounts.length) {
+    return { status: 400, message: '게시글을 생성하는 중에 문제가 발생했습니다.' };
+  }
+
   try {
     const post = await Post.create(postProps).exec();
     return post;
@@ -34,11 +52,27 @@ export async function createPost(postProps) {
 
 export async function updatePost(_id, postDate) {
   try {
-    const data = await Post.updateOne({ _id }, postDate).exec();
+    const result = await Post.updateOne({ _id }, postDate).exec();
 
-    if (!data) {
-      return { status: 200, message: '수정 실패' };
+    if (!result) {
+      return { status: 400, message: '수정 실패' };
     }
+
+    return { status: 200, message: '수정 성공' };
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export async function deletePostById(_id) {
+  try {
+    const result = await Post.deleteOne({ _id }).exec();
+
+    if (!result) {
+      return { status: 400, message: '선택삭제 실패' };
+    }
+
+    return { status: 200, message: '선택삭제 성공' };
   } catch (err) {
     throw new Error(err);
   }
@@ -55,14 +89,6 @@ export async function deleteAllPosts(postList) {
     }
     return;
   } catch (err) {
-    throw new Error('삭제 할 수 없습니다.');
-  }
-}
-
-export async function deletePostById(_id) {
-  try {
-    return await Post.deleteOne({ _id });
-  } catch (err) {
-    throw new Error('삭제 할 수 없습니다.');
+    throw new Error(err);
   }
 }
