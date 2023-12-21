0fd517fea7994a4110da02ebeafe7434ec434f9a
kojeounhwa <rhwidghk125@naver.com>
2023-12-21 11:32:45 +0900

    Fix: postman으로 테스트하면서 게시글 api 수정

--- src/constants/errorConstant.js
+++ src/constants/errorConstant.js
@@ -3,7 +3,9 @@ const commonError = Object.seal({
   UNKNOWN_ERROR: 'Unknown Error',
   POST_UNKNOWN_ERROR: 'Post Unknown Error',
   POST_MODIFY_ERROR: 'Post Modify Error',
-  USER_MATCH_ERROR: 'User Math Error',
+  POST_DELETE_ERROR: 'Post Delete Error',
+  SCHEDULE_MATCH_ERROR: 'Schedule Match Error',
+  USER_MATCH_ERROR: 'User Match Error',
   BOOKMARK_UNKNOWN_ERROR: 'Bookmark Unknown Error',
 });
 
--- src/services/postService.js
+++ src/services/postService.js
@@ -14,7 +14,7 @@ export async function getAllPosts() {
 
 // 특정 게시글 조회
 export async function getPostByPostId(postId) {
-  await Post.findOne(postId).catch((error) => {
+  return await Post.findOne({ _id: postId }).catch((error) => {
     throw new CustomError(commonError.DB_ERROR, 'Internal server error', {
       statusCode: 500,
       cause: error,
@@ -34,21 +34,29 @@ export async function createPost(
   const singleSchedulePlaceCounts = schedules.map((location) => location.length - 1);
   const distancesCounts = distances.map((distance) => distance.length);
 
+  // startDate, endDate Date로 변경
+  const stDate = new Date(startDate);
+  const edDate = new Date(endDate);
+
   // 여행일정 수 계산 (5월 30일 ~ 6월 2일이여도 계산 가능)
-  const dtMs = endDate.getTime() - startDate.getTime();
-  console.log(dtMs);
-  const travelDays = dtMs / (1000 * 60 * 60 * 24);
+  const dtMs = edDate.getTime() - stDate.getTime();
+  const travelDays = dtMs / (1000 * 60 * 60 * 24) + 1;
 
   // 등록된 여행 날짜와 디데이 불일치 또는 세부 장소와 거리 갯수 불일치 시 오류 반환
   if (singleScheduleLength !== travelDays) {
-    throw new Error('등록된 여행 날짜와 여행 일정 수가 일치하지 않습니다.');
+    throw new CustomError(commonError.SCHEDULE_MATCH_ERROR, '등록된 여행 날짜와 여행 일정 수가 일치하지 않습니다', {
+      statusCode: 404,
+    });
   }
 
   if (singleSchedulePlaceCounts.length !== distancesCounts.length) {
-    throw new Error('등록된 여행 장소 갯수와 거리 갯수가 일치하지 않습니다.');
+    throw new CustomError(commonError.SCHEDULE_MATCH_ERROR, '등록된 여행 장소 갯수와 거리 갯수가 일치하지 않습니다.', {
+      statusCode: 404,
+    });
   }
 
-  await Post.create(userId, {
+  return await Post.create({
+    authorId: userId,
     title,
     destination,
     startDate,
@@ -74,69 +82,70 @@ export async function updatePost(
   postId,
   { title, destination, startDate, endDate, tag, schedules, distances, cost, peopleCount, isPublic, reviewText },
 ) {
-  const post = await Post.findById(postId)
-    .populate('authorId')
-    .lean()
-    .catch((error) => {
-      throw new CustomError(commonError.DB_ERROR, 'Internal server error', {
-        statusCode: 500,
-        cause: error,
-      });
-    });
-
   // 작성자와 수정하려는 사용자가 일치한지
-  if (post.authorId !== userId) {
-    throw new CustomError(commonError.USER_MATCH_ERROR, '게시글을 수정할 권한이 없습니다.', {
-      statusCode: 403,
-      cause: error,
+  const post = await Post.findOne({ _id: postId }).lean();
+
+  if (!post) {
+    throw new CustomError(commonError.POST_NOT_FOUND, '게시글을 찾을 수 없습니다.', {
+      statusCode: 404,
     });
   }
 
-  const updatedPost = await Post.updateOne(postId, {
-    title,
-    destination,
-    startDate,
-    endDate,
-    tag,
-    schedules,
-    distances,
-    cost,
-    peopleCount,
-    isPublic,
-    reviewText,
-  }).catch((error) => {
+  if (post.authorId.toString() !== userId) {
+    throw new CustomError(commonError.USER_MATCH_ERROR, '게시글을 수정할 권한이 없습니다.', {
+      statusCode: 403,
+    });
+  }
+
+  const updatedPost = await Post.updateOne(
+    { _id: postId },
+    {
+      title,
+      destination,
+      startDate,
+      endDate,
+      tag,
+      schedules,
+      distances,
+      cost,
+      peopleCount,
+      isPublic,
+      reviewText,
+    },
+  ).catch((error) => {
     throw new CustomError(commonError.DB_ERROR, 'Internal server error', {
       statusCode: 500,
       cause: error,
     });
   });
-
   if (updatedPost.modifiedCount === 0) {
-    throw new CustomError(commonError.POST_MODIFY_ERROR, '게시글 수정을 실패하였습니다.', {
-      statusCode: 404,
-      cause: error,
-    });
+    throw new CustomError(commonError.POST_MODIFY_ERROR, '게시글 수정을 실패하였습니다.', { statusCode: 404 });
   }
+
+  return updatedPost;
 }
 
 // 특정 사용자의 게시글 삭제
 export async function deletePost(userId, postId) {
-  const post = await Post.findOne(postId)
-    .populate('authorId')
-    .lean()
-    .catch((error) => {
-      throw new CustomError(commonError.DB_ERROR, 'Internal server error', {
-        statusCode: 500,
-        cause: error,
-      });
-    });
+  const post = await Post.findOne({ _id: postId }).lean();
 
-  if (post.authorId !== userId) {
-    throw new CustomError(commonError.USER_MATCH_ERROR, '게시글을 삭제할 권한이 없습니다.', {
-      statusCode: 403,
-      cause: error,
+  if (!post) {
+    throw new CustomError(commonError.POST_NOT_FOUND, '게시글을 찾을 수 없습니다.', {
+      statusCode: 404,
     });
   }
 
-  await Post.deleteOne(postId);
+  // 작성자와 삭제하려는 사용자가 일치한지
+  if (post.authorId.toString() !== userId) {
+    throw new CustomError(commonError.USER_MATCH_ERROR, '게시글을 삭제할 권한이 없습니다.', {
+      statusCode: 403,
+    });
+  }
+
+  return await Post.deleteOne({ _id: postId }).catch((error) => {
+    throw new CustomError(commonError.DB_ERROR, 'Internal server error', {
+      statusCode: 500,
+      cause: error,
+    });
+  });
 }
