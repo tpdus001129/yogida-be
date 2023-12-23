e13b94ba1287b565009dd10a11e257388138900e
Dev-koo <devgiyun@gmail.com>
2023-12-23 19:32:48 +0900

    fix: 커스텀 에러와 에러핸들러 수정

--- src/middleware/isAuth.js
+++ src/middleware/isAuth.js
@@ -4,18 +4,20 @@ import CustomError from './errorHandler.js';
 
 export async function isAuth(req, res, next) {
   // 헤더에서 cookie를 가져온다.
-  const token = req.cookies?.token;
-
-  if (!token) {
-    throw new CustomError('Authentication Error', '유효한 토큰이 아닙니다.', { statusCode: 401 });
-  }
-
   try {
+    const token = req.cookies?.token;
+
+    if (!token) {
+      throw new CustomError('Authentication Error', '토큰이 없습니다.', {
+        statusCode: 401,
+      });
+    }
+
     const decoded = verifyToken(token);
 
     const user = await User.findOne({ email: decoded.email }, { password: false }).lean();
     if (!user) {
-      throw new CustomError('Authentication Error', '유효한 토큰이 아닙니다.', { statusCode: 401 });
+      throw new CustomError('Authentication Error', '유저를 찾을 수 없습니다.', { statusCode: 401 });
     }
 
     req.user = user;
